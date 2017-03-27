e3bce7b2e97c4a695b3277a3b02e029a3b5320f8
Cmoadne <535485206@qq.com>
2017-03-27 14:25:35 +0800

    添加活动停止被回收数据保存的代码

--- app/src/main/java/com/example/cmoadne/a3activitylifecycletest/MainActivity.java
+++ app/src/main/java/com/example/cmoadne/a3activitylifecycletest/MainActivity.java
@@ -10,12 +10,20 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
+
     @Override
+    //参数是用于可能的恢复数据
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         Log.d(TAG, "onCreate");
+
+        //有数据需要恢复
+        if (savedInstanceState != null) {
+            String tempData = savedInstanceState.getString("data_key");
+            Log.d(TAG, "onCreate: recover data" + tempData);
+        }
 
         Button startNormalActivity = (Button) findViewById(R.id.button_1_start_normal);
         Button startDialogActivity = (Button) findViewById(R.id.button_1_start_dialog);
@@ -23,14 +31,14 @@ public class MainActivity extends AppCompatActivity {
         startNormalActivity.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this,NormalActivity.class);
+                Intent intent = new Intent(MainActivity.this, NormalActivity.class);
                 startActivity(intent);
             }
         });
         startDialogActivity.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this,DialogActivity.class);
+                Intent intent = new Intent(MainActivity.this, DialogActivity.class);
                 startActivity(intent);
             }
         });
@@ -38,38 +46,47 @@ public class MainActivity extends AppCompatActivity {
 
     //重写这几个方法，方便理解生命周期见 DRAWABLE里面的图片
     @Override
-    protected void onStart(){
+    protected void onStart() {
         super.onStart();
         Log.d(TAG, "onStart");
     }
 
     @Override
-    protected void onResume(){
+    protected void onResume() {
         super.onResume();
         Log.d(TAG, "onResume:");
     }
 
     @Override
-    protected void onPause(){
+    protected void onPause() {
         super.onPause();
         Log.d(TAG, "onPause");
     }
 
     @Override
-    protected void onStop(){
+    protected void onStop() {
         super.onStop();
         Log.d(TAG, "onStop");
     }
 
     @Override
-    protected void onDestroy(){
+    protected void onDestroy() {
         super.onDestroy();
         Log.d(TAG, "onDestroy");
     }
 
     @Override
-    protected void onRestart(){
+    protected void onRestart() {
         super.onRestart();
         Log.d(TAG, "onRestart");
+    }
+
+    //重写活动处于停止状态被回收时候保存状态
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        String savedate = "Something saved1";
+        //参数 键值（用于取回） 保存值
+        outState.putString("data_key", savedate);
     }
 }
